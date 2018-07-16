489abbc26847c1f9008c6efd215337719b093420
zin8szh <external.Xin.Zhang6@cn.bosch.com>
2018-07-16 11:06:15 +0800

    create localizable files

---
+++ MyDemoSwift/UI/MyUIMenuViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MyUIMenuViewController.swift
+//  MyDemoSwift
+//
+//  Created by 张鑫 on 2018/7/16.
+//  Copyright © 2018年 张鑫. All rights reserved.
+//
+
+import UIKit
+
+class MyUIMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
